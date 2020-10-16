9ef203d568f3b3c7c6b2232826c27f1a9e6ab103
Shuvanshu123 <71654876+Shuvanshu123@users.noreply.github.com>
2020-10-16 16:45:09 +0530

    adding routing links of pages 16-20 and after 36

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/',async(req,res)=>{
-    res.render("create.ejs")
+    res.render('create')
 });
 
 app.get('/login',(req,res)=>{
