fcf2592676e8ac5f1eee0f68936346590c379a7c
arul johnson <aruljohnson9411@gmail.com>
2020-11-18 18:05:50 +0530

    user model added

--- walson/jiwesh/models/user.js
+++ walson/jiwesh/models/user.js
@@ -21,7 +21,21 @@ var userSchema = new mongoose.Schema(
       trim: true,
       required: true,
       unique: true
+      validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error('Email not valid')
     },
+    password:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+    validate(value){
+      if(value.length<6){
+        throw new Error()
+      }
+    }
+  },
     age:{
       type:Number,
       require:true
@@ -36,28 +50,23 @@ var userSchema = new mongoose.Schema(
       maxlength: 70,
       trim: true
     },
-    encry_password: {
-      type: String,
-      required: true
-    },
-    salt: String,
-    role: {
-      type: Number,
-      default: 0
-    },
     posts: [{
-      type:mongoose.Schema.id
+      type:mongoose.Schema.ObjectId
       ref:"Posts" 
     }]
   },
     followers: [{
-      type:mongoose.Schema.id
+      type:mongoose.Schema.ObjectId
       ref:"Followers" 
     }]
   ,
-  
+  following: [{
+      type:mongoose.Schema.ObjectId
+      ref:"Following" 
+    }]
+  ,
   inbox: [{
-      type:mongoose.Schema.id
+      type:mongoose.Schema.ObjectId
       ref:"Inbox" 
     }]
   ,
@@ -76,6 +85,42 @@ var userSchema = new mongoose.Schema(
       ref:"Messages" 
     }]
   ,
+  topleader: {
+      type:mongoose.Schema.ObjectId
+      ref:"Topleader" 
+    }
+  ,
+  corusmember: {
+      type:mongoose.Schema.ObjectId
+      ref:"Corusmember" 
+    }
+  ,
+  coruspost: [{
+      type:mongoose.Schema.ObjectId
+      ref:"Coruspost" 
+    }]
+  ,
+  mytop8: [{
+      type:Boolean,
+      ref:"mytop8" 
+    }]
+  ,
+  dating: {
+      type:mongoose.Schema.ObjectId
+      ref:"Followers" 
+    }
+  ,
+  comments: {
+      type:mongoose.Schema.ObjectId
+      ref:"Comments" 
+    }
+  ,
+  notifications: {
+      type:mongoose.Schema.ObjectId
+      ref:"Notifications" 
+    }
+  ,
+
   { timestamps: true }
 );
 module.exports = mongoose.model("User",userSchema);
