978fd64471e5002eafe781b71f45ffcca18a5d01
Shuvanshu123 <71654876+Shuvanshu123@users.noreply.github.com>
2020-11-18 23:01:50 +0530

    platform routes update

--- src/routes/platform-routes.js
+++ src/routes/platform-routes.js
@@ -3,30 +3,36 @@ const app =  express();
 const multer = require('multer')
 const router = express.Router();
 const mongoose = require("mongoose");
-
+const cloudinary = require("cloudinary").v2;
 const Products = require("../models/featuredproducts");
 const Stores = require("../models/featuredstores");
 const fs = require('fs')
 const path = require('path')
 
+cloudinary.config({ 
+    cloud_name: 'shuvanshu', 
+    api_key: '438498144725888', 
+    api_secret: 'TutkX5DDT1tSRil5bbo7g-jvg_U' 
+  });
 
 app.use(express.json())
 
 // GET Routes
 
 router.get('/',(req,res)=>{
-    const products = Products.find()
-    const stores = Stores.find()
+    var query = { isfeatured : true };
+    const products = Products.find(query)
+    const stores = Stores.find(query)
     res.render('platform',{products:products, stores:stores})
 });
 
 router.get('/stores',(req,res)=>{
     const stores = Stores.find()
-    res.render('stores',{})
+    res.render('stores',{stores:stores})
 });
 
 router.get('/addstore',(req,res)=>{
-    res.render('addstore',{})
+    res.render('form',{})
 });
 
 router.get('/addproduct',(req,res)=>{
@@ -36,30 +42,33 @@ router.get('/addproduct',(req,res)=>{
 // POST Routes
 
 router.post('/addstore',(req,res)=>{
-    const {title,link,isfeatured} = req.body
-    var file = req.files
-    var imgarr = [];
-            
-    for(var i=0;i<file.length;i++){
-        var src = file[i].filename;
-        imgarr.push(src);
-    }
-    const newstore = new Stores({title,img:imgarr,link,isfeatured})
+    // console.log(req.files)
+    const {title,link,imgurl,isfeatured} = req.body
+    console.log(req.body);
+    // var file = req.files;
+        var temp=new String;
+        cloudinary.uploader.upload(imgurl,(error,result)=>{
+            console.log(error);
+            console.log(result);
+            temp = result.url;
+        });
+    const newstore = new Stores({title,imgurl:temp,link,isfeatured})
     newstore.save()
+    res.redirect('/platform/addstore');
 });
 
 router.post('/addproduct',(req,res)=>{
-    const {title,price,link,isfeatured} = req.body
-    var file = req.files
-    var imgarr = [];
-            
-    for(var i=0;i<file.length;i++){
-        var src = file[i].filename;
-        imgarr.push(src);
-    }
-    const newproduct = new Products({title,price,img:imgarr,link,isfeatured})
+    const {title,imgurl,price,link,isfeatured} = req.body
+    // var file = req.files
+    var temp=new String;
+        cloudinary.uploader.upload(imgurl,(error,result)=>{
+            // console.log(error);
+            // console.log(result);
+            temp = result.url;
+        });
+    const newproduct = new Products({title,price,img:temp,link,isfeatured})
     newproduct.save()
-    res.redirect('/')
+    res.redirect('/platform')
 });
 
 module.exports = router;
