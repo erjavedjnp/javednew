8c4323a3669acd3611afc75f3993404ec766a1ed
Ashish-ecera <70714183+Ashish-ecera@users.noreply.github.com>
2020-11-21 15:09:08 +0530

    screen33(platform) and screen34(stores)
    both frontend and backend done of screen33 and 34

---
+++ app.js
@@ -0,0 +1,234 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser =  require("body-parser");
+const path = require('path');
+const app = express();
+const userController = require("./src/routes/user");
+const datemeuserController = require("./src/Routes/datemeuser");
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
+const Categories = require('./src/models/categories');
+const Products = require('./src/models/products');
+const Cart = require('./src/models/cart');
+const platformroutes = require("./src/routes/platform-routes")
+const eventroutes = require("./src/routes/event-routes")
+const user = require('./src/models/comment');
+const blogs = require('./src/models/blogs');
+const reco = require('./src/models/reco');
+const { AsyncLocalStorage } = require("async_hooks");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser())
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+// app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
+
+const uri = 'mongodb+srv://shouviksur:shouvik1.@cluster0.xsmvn.mongodb.net/marketplace?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
+
+app.get('/',async(req,res)=>{
+    res.render('create')
+});
+
+app.get('/login',(req,res)=>{
+    res.render('index',{})
+});
+
+app.get('/forget',(req,res)=>{
+    res.render('forget',{})
+});
+
+app.get('/privacy',(req,res)=>{
+    res.render('privacy',{})
+});
+
+
+app.get('/groups',(req,res)=>{
+    res.render('groups',{})
+});
+
+app.get('/groups/:name',(req,res)=>{
+    res.render('group-detail',{})
+});
+
+
+app.get('/register',(req,res)=>{
+    res.render('signup',{})
+});
+
+app.get('/chorus-search',(req,res)=>{
+    res.render('chorus-search',{})
+});
+
+app.get('/product-detail',(req,res)=>{
+    res.render('product-detail',{})
+});
+
+app.use("/events", eventroutes);
+
+app.use("/platform", platformroutes);
+
+app.get('/market-place',async (req,res)=>{
+
+    const categories = await Categories.find()
+    const products=await Products.find()
+    res.render('market-place',{ categories:categories, products:products})
+});
+
+app.get('/product/:id',async (req,res)=>{
+  
+    const category = await Categories.findById(req.params.id)
+    const product=await Products.findById(req.params.id)
+    let item;
+    if(category!=null)
+     item=category;
+  
+    else if(product!=null)
+     item=product;
+    res.render('product-detail',{item:item})
+    
+
+});
+app.get('/cart',async (req,res)=>{
+    const items=await Cart.find()
+    res.render('product-cart',{items:items})
+
+});
+app.post('/product/:id',async (req,res)=>{
+    
+    const category = await Categories.findById(req.params.id)
+    const product=await Products.findById(req.params.id)
+    var item;
+    if(category!=null)
+     item=category;
+  
+    else if(product!=null)
+     item=product;
+
+    let newitem=new Cart({
+        name:item.name,
+        price:item.price,
+        originalprice:item.originalprice,
+        number:1,
+        imgurl:item.imgurl
+    })
+    newitem=await newitem.save()
+    res.redirect('/cart')
+});
+
+app.delete('/cart/:id', async (req, res) => {
+    await Cart.findByIdAndDelete(req.params.id)
+    res.redirect('/cart')
+  });
+  app.put('/cart', async (req, res) => {
+   
+     
+    
+    res.redirect('/cart')
+    
+  });
+
+
+app.get('/blogs',(req,res)=>{
+    res.render('blogs',{})
+});
+
+app.get('/blog/:name',(req,res)=>{
+    console.log(req.params.name)
+    res.render('blog-detail',{})
+});
+app.use('/public', express.static('blog_detail'));
+
+//mine
+app.get('/mak', function(req, res){
+    res.render('blog-detail');
+    });
+//post routes
+app.post('/new', function(req, res){
+new user({
+
+about: req.body.about,
+
+}).save(function(err, doc){
+if(err) res.json(err);
+else res.redirect('/mak')
+});
+});
+
+    
+    //mine
+    //for blogs
+    app.get('/mak2', function(req, res){
+        blogs.find({}, function(err, docs){
+        if(err) res.json(err);
+        else res.render('blogs2', {users: docs});
+        });
+        });
+    //post routes
+    app.post('/new', function(req, res){
+    new blogs({
+    name: req.body.name,
+    lname: req.body.lname,
+    about: req.body.about,
+    photo: req.body.photo,
+    age : req.body.age
+    }).save(function(err, doc){
+    if(err) res.json(err);
+    else res.redirect('/mak2')
+    });
+    });
+
+
+
+
+    //for blogs
+
+    //for recomendation
+
+    
+    //get routes
+    app.get('/mak3', function(req, res){
+        reco.find({}, function(err, docs){
+        if(err) res.json(err);
+        else res.render('swipping2', {users: docs});
+        });
+        });
+    //post routes
+    app.post('/new', function(req, res){
+    new reco({
+        name: req.body.name,
+        lname: req.body.lname,
+        about: req.body.about,
+        photo: req.body.photo,
+        age : req.body.age
+    }).save(function(err, doc){
+    if(err) res.json(err);
+    else res.redirect('/mak3')
+    });
+    });
+    
+
+
+
+    //recomendations
+
+
+app.use("/:user",userController);
+
+app.use("/DateMe/:user",datemeuserController);
+
+
+
+
+app.listen(5000, () =>{
+    console.log("server running at port 5000")
+})
---
+++ src/models/user.js
@@ -0,0 +1,118 @@
+var mongoose = require("mongoose");
+const validator =require('validator')
+// const uuidv1 = require("uuid/v1");
+const jwt=require('jsonwebtoken')
+
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true
+    },
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error('Email not valid');
+    }},
+    password:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+    validate(value){
+      if(value.length<6){
+        throw new Error()
+      }
+    }
+  },
+    age:{
+      type:Number,
+      required:true
+
+    },
+    userinfo: {
+      type: String,
+      trim: true
+    },
+     status: {
+      type: String,
+      maxlength: 70,
+      trim: true
+    },
+    posts: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"Posts" 
+    }]
+  },
+    followers: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"User" 
+    }]
+  ,
+  following: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"User" 
+    }]
+  ,
+  inbox: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"Inbox" 
+    }]
+  ,
+  notification: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"Notifications" 
+    }]
+  ,
+  story: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"Story" 
+    }]
+  ,
+  messages: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"Messages" 
+    }]
+  ,
+  istopleader: {
+      type:Boolean
+      
+    }
+  ,
+  
+  mytop8: [{
+      type:mongoose.Schema.ObjectId,
+      ref:"User" 
+    }]
+  ,
+  dating: {
+      type:mongoose.Schema.ObjectId,
+      ref:"Dating" 
+    }
+  ,
+  comments: {
+      type:mongoose.Schema.ObjectId,
+      ref:"Comment" 
+    }
+  ,
+ 
+
+  createdAt:{
+    type:Number,
+    default:new Date().getTime()
+
+  }
+}
+);
+module.exports = mongoose.model("User",userSchema);
