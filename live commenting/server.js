4b5606f54479047255aedb694f501aa3467c5aea
JayeshEcera <71709255+JayeshEcera@users.noreply.github.com>
2020-11-27 18:10:21 +0530

    Add files via upload

---
+++ live commenting/server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: "1113407",
+  key: "81c5ef61345b1931bfc2",
+  secret: "c550f07eee1e88f474a7",
+  cluster: 'ap2',
+  useTLS: true,
+  encrypted: true
+});
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/comment', function(req, res){
+  console.log(req.body);
+  var newComment = {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment
+  }
+  pusher.trigger('flash-comments', 'new_comment', newComment);
+  res.json({  created: true });
+});
+
+// Error Handler for 404 Pages
+app.use(function(req, res, next) {
+    var error404 = new Error('Route Not Found');
+    error404.status = 404;
+    next(error404);
+});
+
+module.exports = app;
+
+app.listen(9000, function(){
+  console.log('Example app listening on port 9000!')
+});
